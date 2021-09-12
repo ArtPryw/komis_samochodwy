1e4e57dc9d72a297123cb82cb098f42d33d6cca4
ArtPryw <artur.pryw@gmail.com>
2021-09-12 09:45:35 +0200

    dodane motocykle i ciezarowe

---
+++ ciezarowe.py
@@ -0,0 +1,34 @@
+from osobowe import *
+
+class Ciezarowy(Osobowy):
+
+    def __init__(self,cena, max_paliwo, ladownosc):
+        self.ladownosc = ladownosc
+        self.zaladowane = 0
+        super().__init__(cena, max_paliwo)
+
+    def wlacz_silnik(self):
+        print("Uruchamiam samochód ciężarowy")
+        super().wlacz_silnik()
+    def wylacz_silnik(self):
+        print("Wyłączam samochód ciężarowy")
+        super().wylacz_silnik()
+    def tankuj(self, litry):
+        print("Tankuję, samochód ciężrowy")
+        super().tankuj(litry)
+
+    def laduj(self, ladunek):
+        if ladunek < (self.ladownosc - self.zaladowane):
+            print(f"Ładuje ładunek o masie {ladunek}")
+            self.zaladowane = ladunek
+        else:
+            print("nie zmieścimy już tego ładunku.")
+    #TODO: obsłużyć wyładowanie poniżej 0.
+
+    def rozladuj(self, ladunek):
+        print(f"Rozładowuje ładunek o masie {ladunek}")
+        self.zaladowane -= ladunek
+
+    def pokaz_stan_naladowania(self):
+        print(f"Aktualnie załadowane {self.zaladowane} kg.")
+
---
+++ motocykle.py
@@ -0,0 +1,20 @@
+from pojazdy import *
+
+class Motocykl(Pojazdy):
+
+    def __init__(self, cena, silnik_typ, liczba_miejsc):
+        self.cena = cena
+        self.silnik_typ = silnik_typ
+        self.liczba_miejsc = liczba_miejsc
+
+    def cena(self):
+        return self.cena
+
+    def opis(self):
+        return f"Motocyk kosztuje {self.cena}zł i ma {self.liczba_miejsc} miejsca."
+
+    def wlacz_silnik(self):
+        pass
+
+    def wylacz_silnik(self):
+        pass
